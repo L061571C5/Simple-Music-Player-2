32bab811db9375ac3f9e0cb23766e97dabba365f
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-04-22 14:54:35 -0500

    2.1.2

--- Simple Music Player 2/Properties/AssemblyInfo.cs
+++ Simple Music Player 2/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Simple Music Player 2")]
 [assembly: AssemblyDescription("Simple Music Player 2: Electric Boogaloo")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("L061571C5")]
 [assembly: AssemblyProduct("Simple Music Player 2")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
