0e6efa444b49798ae7bc06ff85001af076cfaacf
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-04-28 14:37:59 -0500

    spacing is off bug

--- Simple Music Player 2/BaseForm.cs
+++ Simple Music Player 2/BaseForm.cs
@@ -252,6 +252,10 @@ namespace Simple_Music_Player_2
             volumeTrackBar.Value = Math.Min(100, Math.Max((int)(MusicData.volume * 100), 0)) == volumeTrackBar.Value ? volumeTrackBar.Value : Math.Min(100, Math.Max((int)(MusicData.volume * 100), 0));
             trackTime.Text = TimeSpan.FromMilliseconds(MusicData.posMs).ToString(@"hh\:mm\:ss") + " \\ " + TimeSpan.FromMilliseconds(MusicData.totalMs).ToString(@"hh\:mm\:ss");
             setPresence(MusicData.title, MusicData.artist, MusicData.totalMs - MusicData.posMs, "logo", MusicData.album, soundOut.PlaybackState == PlaybackState.Playing ? true : false);
+
+            relocatelabel(titleText);
+            relocatelabel(artistText);
+            relocatelabel(albumText);
         }
 
         private void viewQueueToolStripMenuItem_Click(object sender, EventArgs e)
